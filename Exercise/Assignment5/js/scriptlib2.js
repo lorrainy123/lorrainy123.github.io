e2d6826c1a146f480fe80bd134e15cafb1931a84
LI Chaoran <lorrainechaoran@163.com>
2018-11-05 16:13:04 +0800

    1

--- Exercise/Assignment5/js/scriptlib2.js
+++ Exercise/Assignment5/js/scriptlib2.js
@@ -60,7 +60,6 @@ $(document).ready(function(){
                     columns: [
                         { title: "Name",
                           defaultContent:""},
-                          defaultContent:""},
                         { title: "Profit",
                           defaultContent:""},
                     ] // rmf columns
